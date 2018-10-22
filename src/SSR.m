2f665da78dbec820850a7046cbdc7db23af88e43
shjain <shjain@PNDw008.d.ethz.ch>
2018-10-22 21:08:53 +0200

    added derivative of Greens function w.r.t. T. The critically damped case still remains

--- src/SSR.m
+++ src/SSR.m
@@ -420,6 +420,53 @@ classdef SSR < handle
             O.isupdated.CML = true;
         end
         
+        function update_dLvec(O)
+            t1 = -O.T:O.dt:O.T; % padding Green's function to ensure correct convolution
+            O.DLvec = dLdT(O,t1,O.T);
+            O.isupdated.dL = true;
+        end
+        
+        function L = dLdT(O,t,T)
+            % t must be a row vector
+            % underdamped
+            om = repmat(O.omega(O.u), size(t));
+            al = repmat(O.alpha(O.u),size(t));
+            exp_alpha_T = repmat(exp(O.alpha(O.u) * T),size(t));
+            exp_alpha_2T = repmat(exp(2*O.alpha(O.u) * O.T),size(t));
+            exp_alpha_t_T_om = exp(O.alpha(O.u) * (t + T))./om;
+            denominator = repmat(1 + exp(2*O.alpha(O.u)*T) -...
+                2 * exp(O.alpha(O.u)*T).* cos(O.omega(O.u)*T), size(t));
+            denominator2 = repmat((1 + exp(2*O.alpha(O.u)*T) -...
+                2 * exp(O.alpha(O.u)*T).* cos(O.omega(O.u)*T)).^2, size(t));
+            Lu = exp_alpha_t_T_om.* ( ( om.* cos(O.omega(O.u)*(T+t)) + ...
+                al.* sin(O.omega(O.u)*(T+t)) - 2*al.* exp_alpha_T.* sin(O.omega(O.u) * t) )./ ...
+                denominator - ( (sin(O.omega(O.u)*(t + T)) - exp_alpha_T.* sin(O.omega(O.u) * t) ).* ...
+                (2*(al.* (exp_alpha_2T - exp_alpha_T.* repmat(cos(O.omega(O.u)*T),size(t)) ) + ...
+                om.* exp_alpha_T.* repmat(sin(O.omega(O.u)*T),size(t)) )) )./(denominator2));
+            
+            % overdamped
+            if ~isempty(O.o)                 
+                EaT = repmat(exp(O.gamma(O.o) * T), size(t));
+                EatT = exp(O.gamma(O.o) * (t + T));
+                EbT = repmat(exp(O.beta(O.o) * T), size(t));
+                EbtT = exp(O.beta(O.o) * (t + T));
+                EabT = repmat(exp((O.gamma(O.o) + O.beta(O.o)) * T), size(t));
+                a = repmat(O.gamma(O.o) , size(t));
+                b = repmat(O.beta(O.o) , size(t));
+                denominator = (1 - EaT - EbT + EabT);
+                Term1 = (b - (a + b).*EaT).* EbtT + ( (a + b).* EbT - a ).* EatT;
+                Term2 =  ( EbtT.* (1 - EaT) - EatT.*(1 - EbT) ).*...
+                    ( a.*EaT + b.*EbT - (a + b).* EabT );
+                Lo = ( Term1./denominator + Term2./(denominator.^2) )./(b-a);
+            else
+                L = Lu;
+                return
+            end
+            L = zeros(O.n,length(t));
+            L(O.u,:) = Lu;
+            L(O.o,:) = Lo;        
+        end
+        
         function Q = get.Qmat(O)
             if ~O.isupdated.Q
                 update_Qmat(O);
@@ -474,6 +521,13 @@ classdef SSR < handle
             etad = zeros(size(phi));
             for j = 1:O.n
                 etad(j,:) = conv(O.weights.* phi(j,:),O.Jvec(j,:),'same');
+            end
+        end
+        
+        function eta = convolution_dT(O,phi)
+            eta = zeros(size(phi));
+            for j = 1:size(eta,1)
+                eta(j,:) = conv(O.weights.* phi(j,:),O.DLvec(j,:),'same');
             end
         end
         
