f7518c4b16c6a2d5b625aea754d334447bdd36b5
shjain <shjain@PNDw008.d.ethz.ch>
2018-10-22 18:01:44 +0200

    added the computation of quasiperiodic response surface

--- examples/ModifiedShawPierreQuasiPeriodic.m
+++ examples/ModifiedShawPierreQuasiPeriodic.m
@@ -8,12 +8,12 @@ clc; close all
 %% Properties
 m = 1;
 k = 1;
-c = 0.3;
+c = 0.02;
 g = 0.5;
 %% Linear Matrices
-C = c*[1 -1;
+C = c*[2 -1;
     -1 2];
-K = k* [1 -1;
+K = k* [2 -1;
     -1 2];
 M = [m 0;
     0 m];
@@ -27,14 +27,14 @@ alpha = 0.01;   % Loading amplitude
 f =  @(t,Omega) alpha*[1; 1]*sin(Omega*t);
 f_theta = @(theta) alpha*[1; 1]*sin(2*pi*theta); 
 
-%% single frequency linear
-Omega = 1; % single frequency
+%% Linear, periodic
+Omega = 0.1; % single frequency
 SS.Omega = Omega;
 SS.f_theta = f_theta;
 SS.nh = 1; % number of harmonics to be used
 SS.m = 50; % number of discretiztion along each coordinate on the Torus
 
-[x_lin_freq, xd_lin_freq] = SS.LinearResponse();
+[x_lin_freq, ~] = SS.LinearResponse();
 
 % verification of the response computed using integral equations
 T = 2*pi./Omega;
@@ -53,18 +53,50 @@ plot(t,z_lin(:,1:2),'DisplayName','ode45')
 hold on;plot(2*pi*SS.theta_set/Omega,x_lin_freq, 'Displayname', 'Integral Eq.' ) 
 grid on; axis tight
 
-% %% multi-frequency, nonlinear
-% S = @(x)[g * x(1)^3; 0];
-% DS = @(x)[3 * g * x(1)^2, 0 ; 0, 0];
-% SS.S = S;           % Setting the nonlinearity
-% SS.Omega = [1, pi]; % Multifrequency
-% SS.nh = 3;  % change number of along each base frequency to capture nonlinearity
-% 
-% % Nonlinear response with Picard iteration
-% [x_picard,xd_picard] = SS.Picard('init',x_lin_freq,'tol',1e-3,'maxiter',20); % 'init', 'maxiter', 'tol' are optional arguments
-% t_picard = SS.t;
-% 
-% % Nonlinear response with Newton--Raphson iteration
-% SS.DS = DS;         % Setting the derivative
-% [x_newton,xd_newton] = SS.NewtonRaphson(); % find out response using Picard iteration
-% t_newton = SS.t;
+%% quasi-periodic, nonlinear
+S = @(x)[g * x(1)^3; 0];
+DS = @(x)[3 * g * x(1)^2, 0 ; 0, 0];
+SS.S = S;           % Setting the nonlinearity
+SS.Omega = [1, pi]; % Multifrequency
+f_theta = @(theta) alpha*[1 1; 0 0]*sin(2*pi*theta); 
+SS.f_theta = f_theta;
+SS.nh = 3;          % change number of along each base frequency to capture 
+                    % nonlinearity
+                    
+[x_lin_freq, xd_lin_freq] = SS.LinearResponse();
+
+% Nonlinear response with Picard iteration
+try
+[x_picard,xd_picard] = SS.Picard('init',x_lin_freq,'tol',1e-6,'maxiter',20); % 'init', 'maxiter', 'tol' are optional arguments
+t_picard = SS.t;
+catch
+end
+% Nonlinear response with Newton--Raphson iteration
+SS.DS = DS;         % Setting the derivative
+[x_newton,xd_newton] = SS.NewtonRaphson(); % find out response using Picard iteration
+t_newton = SS.t;
+
+%% sequential continuation to obtain response surface
+Omega1_range=[0.9 1.05];
+Omega2=linspace(sqrt(3)-0.1,sqrt(3)+0.1,101);
+Resp = nan(101,101);
+for j = 1:length(Omega2)
+    Omega0 = [Omega1_range(1), Omega2(j)];
+    [Omega1_array,cont_sol,picard_flag] = SS.sequential_continuation(Omega1_range,'idx',1,'ncontsteps',50,'Omega0',Omega0);    
+    % compute norm of the solution
+    N = nan(length(Omega1_array),1);
+    for k = 1:length(Omega1_array)
+        if ~isempty(cont_sol{k})
+            N(k) = norm(cont_sol{k},'fro')/size(SS.theta_set,2);
+        end
+    end
+    Resp(j,:) = N;
+end
+
+figure; surf(Omega1_array, Omega2,Resp)
+xlabel('$$\Omega_1$$ [rad/s] ')
+ylabel('$$\Omega_2$$ [rad/s] ')
+zlabel('$$\|\mathbf{z}\|_2$$ ')
+
+
+
