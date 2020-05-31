8cba421834386d45d354e832463ace04b9da2836
Shobhit <shobhitlives@gmail.com>
2020-05-31 22:04:05 +0200

    Ensuring periodicity of the Green's function
    This formulation avoids redundant calculations and ensures T-periodicity of the Green's function for the grid defined over the interval [-T, T]
    - With the previous formulation, in the highly over-damped cases, we obtained NaN expressions when evaluating the Green's function for negative values of t.

--- @SSR/update_Jvec.m
+++ @SSR/update_Jvec.m
@@ -1,5 +1,9 @@
 function update_Jvec(O)
-t1 = -O.T:O.dt:O.T; % padding Green's function to ensure correct convolution
-O.Jvec = J(O,t1,O.T);
+% Computing Green's function over grid [0,T]
+J1 = J(O,O.t,O.T);
+% Padding Green's function, i.e., using values over grid [-T,T] 
+% to ensure correct convolution (using the fact that the Green's function 
+% is T periodic) 
+O.Jvec = [J1(:,1:end-1) J1];
 O.isupdated.J = true;
 end
