993bf1540db4100adf1e10a62c19a2676a7dffa0
thuthoma <thuthoma@student.ethz.ch>
2020-10-05 15:42:24 +0200

    Update LinearResponse.m
    Added first order implementation of the LinearResponse function.
    
    The output is somewhat overloaded now. In the first order case the output is still called x in the function, but now refers to the vector z in the paper containing positions and velocities. To make this clear, the quantity z is explicitly assigned to x before outputting.

--- @SSR/LinearResponse.m
+++ @SSR/LinearResponse.m
@@ -1,16 +1,35 @@
 function [x,xd] = LinearResponse(O)
-switch O.type
-    case 'p'
-        % Compute modal forcing
-        phi = O.U.' * O.Ft;
-        % Compute modal response by convolution with Green's function
-        eta = convolution_x(O,phi);
-        etad = convolution_xd(O,phi);
-        x = O.U*eta;
-        xd = O.U*etad;
-    case 'qp'
-        x_kappa = reshape(O.Qmat * O.f_kappa(:),O.n,[]);
-        x = real(x_kappa*O.Einv);
-        xd = real(x_kappa*O.Evinv);
-end
+
+switch O.sys_order
+    case 'first'
+        switch O.type
+            case 'p'
+                % Compute modal forcing
+                phi = O.Vinv * O.Ft;
+                % Compute modal response by convolution with Green's function
+                eta = convolution_x(O,phi);
+                z = O.V*eta;
+            case 'qp'
+                
+                z_kappa = reshape(O.Hmat * O.F_kappa(:),O.N,[]);
+                z = real(z_kappa*O.Einv);
+                
+                x  = z; %Outputs the vector z that contains positions and velocities
+                xd = [];
+        end
+    case 'second'
+        switch O.type
+            case 'p'
+                % Compute modal forcing
+                phi = O.U.' * O.Ft;
+                % Compute modal response by convolution with Green's function
+                eta = convolution_x(O,phi);
+                etad = convolution_xd(O,phi);
+                x = O.U*eta;
+                xd = O.U*etad;
+            case 'qp'
+                x_kappa = reshape(O.Qmat * O.f_kappa(:),O.n,[]);
+                x = real(x_kappa*O.Einv);
+                xd = real(x_kappa*O.Evinv);
+        end
 end
