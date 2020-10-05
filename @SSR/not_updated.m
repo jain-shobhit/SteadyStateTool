d0d16cf8a04391281b15ccc61a8e392306abb391
thuthoma <thuthoma@student.ethz.ch>
2020-10-05 15:55:54 +0200

    Added/Updated not_updated functions
    Added the first order implementation in the functions that make sure quantities get updated

--- @SSR/not_updated.m
+++ @SSR/not_updated.m
@@ -1,18 +1,38 @@
 function not_updated(O)
-switch O.type
-    case 'p'
-        update.L = false;
-        update.J = false;
-        update.CML = false;
-        update.DL = false;
-        update.Ft = false;
-        O.isupdated = update;
-    case 'qp'
-        update.Q = false;
-        update.kappa = false;
-        update.theta = false;
-        update.f_kappa = false;
-        update.E    = false;
-        O.isupdated = update;
-end
+
+
+switch O.sys_order
+    case 'first'
+        switch O.type
+            case 'p'
+                update.G = false;
+                update.CML = false;
+                update.Ft = false;
+                O.isupdated = update;
+            case 'qp'
+                update.H = false;
+                update.kappa = false;
+                update.theta = false;
+                update.F_kappa = false;
+                update.E    = false;
+                O.isupdated = update;
+        end
+    case 'second'
+        
+        switch O.type
+            case 'p'
+                update.L = false;
+                update.J = false;
+                update.CML = false;
+                update.DL = false;
+                update.Ft = false;
+                O.isupdated = update;
+            case 'qp'
+                update.Q = false;
+                update.kappa = false;
+                update.theta = false;
+                update.f_kappa = false;
+                update.E    = false;
+                O.isupdated = update;
+        end
 end
